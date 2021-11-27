15b998fcd6065dec50202e2d51d45c22f168df95
trinddade29@gmail.com <trinddade29@gmail.com>
2021-11-27 19:30:16 -0300

    add interface interface Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,13 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
-
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
 
 interface GetImageResponse {
   after: string;
