045ab9d9bb4f519dff8c7629a4c14f684555a0d9
trinddade29@gmail.com <trinddade29@gmail.com>
2021-12-01 23:10:30 -0300

    add new button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,7 +66,14 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
 
+          </Button>
         )}
       </Box>
     </>
