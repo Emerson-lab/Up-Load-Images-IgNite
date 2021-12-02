bfb50d93bef5963e7c7603146a075d758843c20f
trinddade29@gmail.com <trinddade29@gmail.com>
2021-12-02 19:01:46 -0300

    remove commit

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -3,7 +3,7 @@ import fauna from 'faunadb';
 
 const { query } = fauna;
 const client = new fauna.Client({ secret: process.env.FAUNA_API_KEY });
-//coment
+
 interface ImagesQueryResponse {
   after?: {
     id: string;
